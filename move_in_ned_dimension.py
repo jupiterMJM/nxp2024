af4370f24b8b9a44d2393d67509584fd775cc2d6
Maxence B <maxence.barre@student-cs.fr>
2024-09-08 18:35:06 +0200

    sauvegarde de la trajectoire, changement de noms et rtl après la mission

--- move_in_ned_dimension.py
+++ move_in_ned_dimension.py
@@ -6,6 +6,8 @@ Caveat when attempting to run the examples in non-gps environments:
 `drone.offboard.stop()` will return a `COMMAND_DENIED` result because it
 requires a mode switch to HOLD, something that is currently not supported in a
 non-gps environment.
+
+refer to following website to understand the algorithm : https://discuss.px4.io/t/offboard-mode-trajectory-setpoint/32850
 """
 
 import asyncio
@@ -14,9 +16,10 @@ from mavsdk import System
 from mavsdk.offboard import (OffboardError, PositionNedYaw)
 import time
 import numpy as np
+import json
 
 
-async def move_in_ned_with_velocity(drone:System, aiming_pos, velocity, tolerance=0.2):
+async def move_in_ned_with_velocity(drone:System, aiming_pos, velocity, tolerance=0.2, save_trajectory=True):
     """
     aiming_pos: la position RELATIVE au drone actuellement sous (pos_sn, pos_we, pos_hb)
     cad que pour (x, y, z) le drone ira à x mètre vers le QUOI, y metre vers le QUOI, z metre vers le QUOI
@@ -26,17 +29,23 @@ async def move_in_ned_with_velocity(drone:System, aiming_pos, velocity, toleranc
     async for position_ned in drone.telemetry.position_velocity_ned():
         drone_position_init = position_ned.position
         break
-
+    historic = {"north":list(), "east":list(), "down":list(), "speed":list()}
     drone_position_aim = (drone_position_init.north_m + aiming_pos[0], drone_position_init.east_m + aiming_pos[1], drone_position_init.down_m + aiming_pos[2])
     
     prev_time = time.time()
     # puis on boucle jusqu'à ce qu'on y soit
     async for position_ned in drone.telemetry.position_velocity_ned():
         drone_position_current = position_ned.position
-        speed = position_ned.velocity
+        speed = np.array([position_ned.velocity.north_m_s, position_ned.velocity.east_m_s, position_ned.velocity.down_m_s])
         drone_position_current = np.array([drone_position_current.north_m, drone_position_current.east_m, drone_position_current.down_m])
         vecteur_dir = np.array([drone_position_aim[i] - drone_position_current[i] for i in range(3)])
+        print(np.linalg.norm(vecteur_dir))
         if np.linalg.norm(vecteur_dir) < tolerance:
+            if save_trajectory:
+                with open("trajectory.json", "w") as f:
+                    print(historic)
+                    json.dump(historic, f)
+                print("[INFO] Trajectory saved")
             break
         vecteur_unit = vecteur_dir / np.linalg.norm(vecteur_dir)
 
@@ -45,6 +54,12 @@ async def move_in_ned_with_velocity(drone:System, aiming_pos, velocity, toleranc
         next_position = drone_position_current + vecteur_unit * velocity
         await drone.offboard.set_position_ned(PositionNedYaw(next_position[0], next_position[1], next_position[2], 0.0))
         prev_time = time.time()
+
+        if save_trajectory:
+            historic["north"].append(position_ned.position.north_m)
+            historic["east"].append(position_ned.position.east_m)
+            historic["down"].append(position_ned.position.down_m)
+            historic["speed"].append(np.linalg.norm(speed))
 
 
 
@@ -88,15 +103,23 @@ async def run():
         await drone.action.disarm()
         return
 
-
-    await move_in_ned_with_velocity(drone, (-4, 2, 0), 10)
-    await asyncio.sleep(3)
-    print("-- Stopping offboard")
     try:
+        await move_in_ned_with_velocity(drone, (-4, 2, 0), 5)
+        await asyncio.sleep(3)
+        print("-- Stopping offboard")
         await drone.offboard.stop()
     except OffboardError as error:
         print(f"Stopping offboard mode failed \
                 with error code: {error._result.result}")
+    finally:
+        print("[INFO] Returning to launch")
+        await drone.action.return_to_launch()
+        
+        async for in_air in drone.telemetry.in_air():
+            if not in_air:
+                print("[INFO] Disarming")
+                await drone.action.disarm()
+                return
 
 
 if __name__ == "__main__":
